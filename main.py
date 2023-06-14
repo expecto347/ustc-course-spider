3a872ae8138e07c7c06e9778bdc3ae56cea45fb3
Harry <80935331+expecto347@users.noreply.github.com>
2023-06-15 00:31:36 +0800

    1

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import argparse
-keys=['astar.c*','csp.c*','input0.txt','input1.txt'] #这里放文件名中要有的关键字
+keys=[hw?.pdf] #这里放文件名中要有的关键字
 search='ustc' #这里放搜索关键字
 parser = argparse.ArgumentParser(description='Ustc-course Spyder')
 parser.add_argument('token', help='token for your github account', type=str)
